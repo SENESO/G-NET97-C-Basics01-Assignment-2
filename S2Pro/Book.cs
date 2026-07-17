70609d86c72611733cd8066d450f5d848b88722a
Eslam Ashraf <ElmistRo@outlook.com>
2026-07-18 00:33:49 +0300

    Question01

---
+++ S2Pro/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S2Pro
+{
+    internal class Book
+    {
+        public string Title;
+        public int Pages;
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, Pages: {Pages}";
+        }
+    }
+}
+
--- S2Pro/Program.cs
+++ S2Pro/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace S2Pro
+﻿
+using S2Pro;
+
+namespace S2Pro
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            /*
+            // create new class named "book" and using "using S2Pro;" to access it in the main program
+
+            Book book = new Book();
+            book.Title = "C# Fundamentals"; // Assign a title to the book
+            book.Pages = 464; // Assign the number of pages to the book
+            
+            
+            object obj =  book; // Assign the book object to an object variable
+            Console.WriteLine(obj); // Print the book object, which will call the ToString() method of the Book class
+            */
+            #endregion
         }
     }
 }
